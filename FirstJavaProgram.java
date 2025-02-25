22f3408203d2733702c35b692aad08ced1c44573
madhav1602 <102716468+madhav1602@users.noreply.github.com>
2025-02-25 14:08:46 +0530

    Update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 class FirstJavaProgram{
 public static void main(String[] args){
-	// System.out.print("Starting with Capgemini training tommorrow");
+	System.out.print("Starting with Chitkara training tommorrow");
 }
 }
